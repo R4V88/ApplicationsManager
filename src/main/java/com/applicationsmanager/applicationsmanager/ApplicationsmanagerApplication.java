fc67d725f3dfb6062e4424ca36e51b5f6561e9a9
Rafał Sochacki <sochacki.rfl@gmail.com>
2022-05-29 16:16:20 +0200

    initial commit

---
+++ src/main/java/com/applicationsmanager/applicationsmanager/ApplicationsmanagerApplication.java
@@ -0,0 +1,13 @@
+package com.applicationsmanager.applicationsmanager;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ApplicationsmanagerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ApplicationsmanagerApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/applicationsmanager/applicationsmanager/ApplicationsmanagerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.applicationsmanager.applicationsmanager;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApplicationsmanagerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
