fc41c2cc34bfaa5f62588c0c35b1e0958e6fa805
Rafał Sochacki <sochacki.rfl@gmail.com>
2022-05-29 16:49:37 +0200

    Appplication Entity and properties

---
+++ src/main/java/com/applicationsmanager/applicationsmanager/applications/entity/Application.java
@@ -0,0 +1,46 @@
+package com.applicationsmanager.applicationsmanager.applications.entity;
+
+import lombok.AccessLevel;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+import lombok.experimental.FieldDefaults;
+import org.springframework.data.annotation.CreatedDate;
+import org.springframework.data.annotation.LastModifiedDate;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.Id;
+import javax.persistence.Version;
+import java.time.LocalDateTime;
+
+@Getter
+@Setter
+@Entity
+@NoArgsConstructor
+@FieldDefaults(level = AccessLevel.PRIVATE)
+public class Application {
+    @Id
+    @GeneratedValue
+    Long id;
+
+    String name;
+
+    String content;
+
+    @Version
+    Long version;
+
+    Long uuid;
+
+    @CreatedDate
+    LocalDateTime createdAt;
+
+    @LastModifiedDate
+    LocalDateTime updatedAt;
+
+    public Application(String name, String content) {
+        this.name = name;
+        this.content = content;
+    }
+}
